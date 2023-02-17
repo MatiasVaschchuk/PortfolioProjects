d651c63dcf44f32c1289b88cd2fced32baddaa68
MatiasVaschchuk <125699719+MatiasVaschchuk@users.noreply.github.com>
2023-02-17 16:25:21 -0600

    Add files via upload

--- AirBnB-Project- SQL.sql
+++ AirBnB-Project- SQL.sql
@@ -1,24 +1,33 @@
 USE [Portfolio Projects]
 GO
 
+------"Top 10 AirBnBs in Madrid (2016) with the highest revenue per year."
+
 SELECT TOP 10
 id, name,price,  price * availability_365 AS rev_365
 FROM   listing$
-ORDER BY rev_365 DESC
+ORDER BY rev_365 DESC;
 
+------"Type of Room in AirBnB Listings in Madrid, Spain (2016)"
 
 SELECT DISTINCT room_type
-FROM listing$
+FROM listing$;
+
+------TOP 3 "Top 3 Airbnbs in Madrid (Spain) with the highest number of reviews-2016."
 
 SELECT TOP 3
 id, name, price, availability_365,neighbourhood_group, number_of_reviews
 FROM listing$
 ORDER BY number_of_reviews DESC;
 
+------"The 5 most expensive AirBnBs in Madrid, Spain in 2016."
+
 SELECT TOP 5
 name, id, neighbourhood_group, price
 FROM listing$
 ORDER BY price DESC;
+
+------"The 10 most in-demand and frequently fully-booked Airbnbs of the year (2016)."
 
 SELECT TOP 10
 id, name,  availability_365, neighbourhood_group, price
